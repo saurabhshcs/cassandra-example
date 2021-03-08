013291809510e8a174ef13c1da7ed2156866e1ea
Saurabh Sharma <saurabhs@motabilityoperations.co.uk>
2021-03-08 01:02:01 +0000

    Initial project commit

---
+++ src/main/java/com/techsharezone/cassandra/crud/example/CassandraCrudExampleApplication.java
@@ -0,0 +1,13 @@
+package com.techsharezone.cassandra.crud.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CassandraCrudExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CassandraCrudExampleApplication.class, args);
+	}
+
+}
